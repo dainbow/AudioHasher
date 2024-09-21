507475be88dad3decd43e563d4c3fcccd6a3cc2c
dainbow <suslov9876@gmail.com>
2024-09-21 20:10:41 +0300

    New version

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use blake2::{Blake2s256, Digest};
 use clap::{Parser, Subcommand};
 use file_hashing::get_hash_file;
 use serde::{Deserialize, Serialize};
-use std::io::prelude::*;
+use skyscraper::xpath::XpathItemTree;
+use skyscraper::{html, xpath};
+use std::io::{self, prelude::*};
 use std::{collections::HashMap, fs::File, path::Path};
 
 #[derive(Parser)]
@@ -15,8 +17,8 @@ struct Args {
 #[derive(Subcommand, Debug, Clone)]
 enum Commands {
     Generate {
-        #[arg(default_value = ".")]
-        answers_dir: String,
+        #[arg(default_value = "./answers.html")]
+        html_path: String,
     },
     Solve {
         #[arg(default_value = "./answers.json")]
@@ -27,36 +29,81 @@ enum Commands {
 }
 
 #[derive(Serialize, Deserialize, Default)]
-struct Answers(HashMap<String, Vec<String>>);
+struct Answers(HashMap<String, Vec<(String, String)>>);
 
 fn main() {
     let args = Args::parse();
 
     match args.cmd {
-        Commands::Generate { answers_dir } => {
-            let path = Path::new(&answers_dir);
+        Commands::Generate { html_path } => {
             let mut answers = Answers::default();
+            let mut file = File::open(html_path).unwrap();
+            let mut contents = String::new();
+            let _ = file.read_to_string(&mut contents);
 
-            for audio in path
-                .read_dir()
-                .expect("Generate subcommand should take a directory as answers_dir")
-            {
-                if let Ok(entry) = audio {
-                    let audio_path = entry.path();
-                    if let Some(file_ext) = audio_path.extension() {
-                        if file_ext == "mp3" {
-                            let mut hash = Blake2s256::new();
+            let document = html::parse(&contents).unwrap();
+            let xpath_item_tree = XpathItemTree::from(&document);
 
-                            let audio_hash = get_hash_file(&audio_path, &mut hash).unwrap();
+            let xpath =
+                xpath::parse("/html/body/div[2]/div[2]/div/div/section[1]/div[1]/form/div/div")
+                    .unwrap();
+            let questions_amount = xpath.apply(&xpath_item_tree).unwrap().len();
 
-                            let prev_vec = answers.0.entry(audio_hash).or_insert(vec![]);
-                            prev_vec.push(
-                                audio_path
-                                    .file_stem()
+            for i in 1..questions_amount {
+                let audio_path = format!("/html/body/div[2]/div[2]/div/div/section[1]/div[1]/form/div/div[{}]/div[2]/div/div[1]/div/div/div/div/div/div[4]/a", i);
+                let xpath = xpath::parse(&audio_path).unwrap();
+                let audio_name: String = xpath.apply(&xpath_item_tree).unwrap()[0]
+                    .extract_as_node()
+                    .extract_as_tree_node()
+                    .data
+                    .extract_as_element_node()
+                    .get_attribute("href")
+                    .unwrap()
+                    .chars()
+                    .take_while(|c| *c != '?')
+                    .collect();
+                let audio_name = audio_name
+                    .split('/')
+                    .last()
+                    .unwrap()
+                    .to_owned()
+                    .replace("%20", " ");
+                let mut hash = Blake2s256::new();
+
+                let audio_hash = get_hash_file(&audio_name, &mut hash).unwrap();
+                let prev_vec = answers.0.entry(audio_hash).or_insert(vec![]);
+
+                let xpath =
+                xpath::parse(&format!("/html/body/div[2]/div[2]/div/div/section[1]/div[1]/form/div/div[{}]/div[2]/div/div[2]/p", i))
+                    .unwrap();
+                let answer_sections_amount = xpath.apply(&xpath_item_tree).unwrap().len();
+
+                for section in 1..=answer_sections_amount {
+                    let xpath =
+                        xpath::parse(&format!("/html/body/div[2]/div[2]/div/div/section[1]/div[1]/form/div/div[{}]/div[2]/div/div[2]/p[{}]/span", i, section))
+                            .unwrap();
+                    let answers_amount = xpath.apply(&xpath_item_tree).unwrap().len();
+                    for ans in 1..=answers_amount {
+                        let answers_path = format!("/html/body/div[2]/div[2]/div/div/section[1]/div[1]/form/div/div[{}]/div[2]/div/div[2]/p[{}]/span[{}]", i, section, ans);
+                        let xpath = xpath::parse(&answers_path)
+                            .unwrap()
+                            .apply(&xpath_item_tree)
+                            .unwrap();
+                        let res = xpath[0].extract_as_node().extract_as_tree_node();
+                        if let Some(answer_type) =
+                            res.data.extract_as_element_node().get_attribute("class")
+                        {
+                            prev_vec.push((
+                                if answer_type.contains("incorrect") {
+                                    "WRONG".to_owned()
+                                } else {
+                                    "OK".to_owned()
+                                },
+                                res.text(&xpath_item_tree)
                                     .unwrap()
-                                    .to_string_lossy()
-                                    .into_owned(),
-                            );
+                                    .to_owned()
+                                    .replace("&nbsp;", ""),
+                            ));
                         }
                     }
                 }
